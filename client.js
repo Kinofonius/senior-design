80e09d4f419b24bca69962eadeeee7bb2e83c484
Kinofonius <78010377+Kinofonius@users.noreply.github.com>
2023-03-23 20:09:43 -0400

    Initial commit

---
+++ client.js
@@ -0,0 +1,114 @@
+const { load } = require('protobufjs');
+const axios = require('axios');
+
+const SERVER_URL = 'http://localhost:3000/rpc';
+
+let RpcMessage;
+let RpcType;
+let GetSceneListResponse;
+let GetSceneResponse;
+let SetSceneResponse;
+
+async function run() {
+  const root = await load('stage.proto');
+  RpcMessage = root.lookupType('stage.v1.RpcMessage');
+  RpcType = root.lookupEnum('stage.v1.RpcType');
+  const GetSceneListRequest = root.lookupType('stage.v1.GetSceneListRequest');
+  GetSceneListResponse = root.lookupType('stage.v1.GetSceneListResponse');
+
+  // Test GetScene request
+  const GetSceneRequest = root.lookupType('stage.v1.GetSceneRequest');
+  GetSceneResponse = root.lookupType('stage.v1.GetSceneResponse');
+
+  const getSceneRequest = GetSceneRequest.create({ scene: 'scene-id' });
+  const rpcMessageGetScene = RpcMessage.create({
+    type: RpcType.RPC_TYPE_REQUEST,
+    id: 2,
+    method: 'GetScene',
+    body: GetSceneRequest.encode(getSceneRequest).finish(),
+  });
+
+  await sendRequest(rpcMessageGetScene);
+
+  // Test SetScene request
+  const SetSceneRequest = root.lookupType('stage.v1.SetSceneRequest');
+  SetSceneResponse = root.lookupType('stage.v1.SetSceneResponse');
+
+  const setSceneRequest = SetSceneRequest.create({
+    uuid: 'new-scene-id',
+    scene: {
+      name: 'New Scene',
+      external: false,
+      stage: {
+        fixtures: [
+          {
+            id: 1,
+            channels: Uint8Array.from([0, 0, 255, 255, 255, 255]),
+          },
+        ],
+      },
+    },
+  });
+
+  const rpcMessageSetScene = RpcMessage.create({
+    type: RpcType.RPC_TYPE_REQUEST,
+    id: 3,
+    method: 'SetScene',
+    body: SetSceneRequest.encode(setSceneRequest).finish(),
+  });
+
+  await sendRequest(rpcMessageSetScene);
+
+  // Example: Sending a GetSceneList request
+  const getSceneListRequest = GetSceneListRequest.create();
+  const rpcMessage = RpcMessage.create({
+    type: RpcType.RPC_TYPE_REQUEST,
+    id: 1,
+    method: 'GetSceneList',
+    body: GetSceneListRequest.encode(getSceneListRequest).finish(),
+  });
+
+  await sendRequest(rpcMessage);
+}
+
+async function sendRequest(rpcMessage) {
+  try {
+    const encodedRpcMessage = RpcMessage.encode(rpcMessage).finish();
+    const response = await axios.post(SERVER_URL, encodedRpcMessage, {
+      responseType: 'arraybuffer',
+      headers: {
+        'Content-Type': 'application/octet-stream',
+      },
+    });
+
+    const responseData = new Uint8Array(response.data);
+    const responseRpcMessage = RpcMessage.decode(responseData);
+    console.log('responseRpcMessage:', responseRpcMessage);
+
+    if (responseRpcMessage.type === RpcType.values.RPC_TYPE_RESPONSE) {
+      let responseBody;
+      switch (responseRpcMessage.method) {
+        case 'GetSceneList':
+          responseBody = responseRpcMessage.body.length ? GetSceneListResponse.decode(responseRpcMessage.body) : { scenes: [] };
+          break;
+        case 'GetScene':
+          responseBody = responseRpcMessage.body.length ? GetSceneResponse.decode(responseRpcMessage.body) : { scene: {} };
+          break;
+        case 'SetScene':
+          responseBody = responseRpcMessage.body.length ? SetSceneResponse.decode(responseRpcMessage.body) : { scene: {} };
+          break;
+        default:
+          throw new Error(`Unknown method ${responseRpcMessage.method}`);
+      }
+      console.log('Received response:', responseRpcMessage, 'with decoded body:', responseBody);
+    } else if (responseRpcMessage.type === RpcType.values.RPC_TYPE_RESPONSE_ERROR) {
+      console.error('Error response:', responseRpcMessage);
+    } else {
+      console.error('Unexpected response:', responseRpcMessage);
+    }
+  } catch (error) {
+    console.error('Error while sending request:', error);
+  }
+}
+
+run().catch((err) => console.log(err));
---
+++ server.js
@@ -0,0 +1,182 @@
+const express = require('express');
+const { loadSync } = require('protobufjs');
+
+const app = express();
+const port = 3000;
+
+run().catch((err) => console.log(err));
+
+async function run() {
+  const root = loadSync('stage.proto');
+  // const ControlService = root.lookupService('stage.v1.ControlService');
+  const Scene = root.lookupType('stage.v1.Scene');
+  const Fixture = root.lookupType('stage.v1.Fixture');
+  const GetSceneListRequest = root.lookupType('stage.v1.GetSceneListRequest');
+  const GetSceneListResponse = root.lookupType('stage.v1.GetSceneListResponse');
+  const GetSceneRequest = root.lookupType('stage.v1.GetSceneRequest');
+  const GetSceneResponse = root.lookupType('stage.v1.GetSceneResponse');
+  const SetSceneRequest = root.lookupType('stage.v1.SetSceneRequest');
+  const SetSceneResponse = root.lookupType('stage.v1.SetSceneResponse');
+  const GetCurrentSceneRequest = root.lookupType('stage.v1.GetCurrentSceneRequest');
+  const GetCurrentSceneResponse = root.lookupType('stage.v1.GetCurrentSceneResponse');
+  const SetCurrentSceneRequest = root.lookupType('stage.v1.SetCurrentSceneRequest');
+  const SetCurrentSceneResponse = root.lookupType('stage.v1.SetCurrentSceneResponse');
+  const GetStateRequest = root.lookupType('stage.v1.GetStateRequest');
+  const GetStateResponse = root.lookupType('stage.v1.GetStateResponse');
+  const SetFixtureRequest = root.lookupType('stage.v1.SetFixtureRequest');
+  const SetFixtureResponse = root.lookupType('stage.v1.SetFixtureResponse');
+  const RpcMessage = root.lookupType('stage.v1.RpcMessage');
+  const RpcType = root.lookupEnum('stage.v1.RpcType');
+
+  // Simulated data
+  let state = {
+    fixtures: [
+      {
+        id: 1,
+        channels: Uint8Array.from([0, 0, 0, 255, 255, 255]),
+      },
+      {
+        id: 2,
+        channels: Uint8Array.from([255, 0, 0, 0, 255, 0]),
+      },
+      {
+        id: 3,
+        channels: Uint8Array.from([0, 255, 0, 0, 0, 255]),
+      },
+    ],
+  };
+
+  app.use(express.raw({ type: 'application/octet-stream' }));
+
+  app.post('/rpc', async (req, res) => {
+    const requestBuffer = req.body;
+    let response;
+    let message;
+  // Decode the incoming message based on its type
+  try {
+    message = RpcMessage.decode(requestBuffer);
+  } catch (error) {
+    console.error('Error while decoding RpcMessage:', error);
+    res.status(400).send('Error while decoding RpcMessage');
+  }
+
+  switch (message.method) {
+    case 'GetSceneList':
+      response = RpcMessage.create({
+        type: RpcType.values.RPC_TYPE_RESPONSE,
+        id: message.id,
+        method: message.method,
+        body: GetSceneListResponse.encode({
+          scenes: [],
+        }).finish(),
+      });
+      break;
+      case 'GetScene':
+          const { scene } = GetSceneRequest.decode(message.body);
+          response = RpcMessage.create({
+            type: RpcType.values.RPC_TYPE_RESPONSE, // RPC_TYPE_RESPONSE
+            id: message.id,
+            method: message.method,
+            body: GetSceneResponse.encode({
+              scene: {
+                id: 'scene-id',
+                name: 'Scene Name',
+                external: false,
+                stage: state,
+              },
+            }).finish(),
+          });
+          break;
+      case 'SetScene':
+          const { uuid, scene: sceneData } = SetSceneRequest.decode(message.body);
+          if (sceneData.external) {
+            response = RpcMessage.create({
+              type: RpcType.values.RPC_TYPE_RESPONSE, // RPC_TYPE_RESPONSE_ERROR
+              id: message.id,
+              method: message.method,
+              body: Buffer.from('Cannot change external scenes'),
+            });
+          } else {
+            state = sceneData.stage;
+            response = RpcMessage.create({
+              type: RpcType.values.RPC_TYPE_RESPONSE, // RPC_TYPE_RESPONSE
+              id: message.id,
+              method: message.method,
+              body: SetSceneResponse.encode({
+                scene: {
+                  id: uuid,
+                  name: sceneData.name,
+                  external: sceneData.external,
+                  stage: state,
+                },
+              }).finish(),
+            });
+          }
+          break;
+      case 'GetCurrentScene':
+          response = RpcMessage.create({
+            type: RpcType.values.RPC_TYPE_RESPONSE, // RPC_TYPE_RESPONSE
+            id: message.id,
+            method: message.method,
+            body: GetCurrentSceneResponse.encode({
+              scene: {
+                id: 'scene-id',
+                name: 'Scene Name',
+                external: false,
+                stage: state,
+              },
+            }).finish(),
+          });
+          break;
+      case 'SetFixture':
+          const { scene: sceneId, fixture } = SetFixtureRequest.decode(message.body);
+          const targetScene = state;
+          const targetFixtureIndex = targetScene.fixtures.findIndex((f) => f.id === fixture.id);
+          if (targetFixtureIndex === -1) {
+            response = RpcMessage.create({
+              type: RpcType.values.RPC_TYPE_RESPONSE, // RPC_TYPE_RESPONSE_ERROR
+              id: message.id,
+              method: message.method,
+              body: Buffer.from(`Fixture with ID ${fixture.id} not found in scene ${sceneId}`),
+            });
+          } else if (targetScene.external) {
+            response = RpcMessage.create({
+              type: RpcType.values.RPC_TYPE_RESPONSE, // RPC_TYPE_RESPONSE_ERROR
+              id: message.id,
+              method: message.method,
+              body: Buffer.from('Cannot modify fixtures in external scenes'),
+            });
+          } else {
+            const newFixture = Fixture.create({
+              type: RpcType.values.RPC_TYPE_RESPONSE,
+              id: fixture.id,
+              method: message.method,
+              channels: fixture.channels,
+            });
+            targetScene.fixtures[targetFixtureIndex] = newFixture;
+            response = RpcMessage.create({
+              type: RpcType.values.RPC_TYPE_RESPONSE, // RPC_TYPE_RESPONSE
+              id: message.id,
+              method: message.method,
+              body: SetFixtureResponse.encode({
+                fixture: newFixture,
+              }).finish(),
+            });
+          }
+          break;
+      default:
+          response = RpcMessage.create({
+            type: RpcType.values.RPC_TYPE_RESPONSE, // RPC_TYPE_RESPONSE_ERROR
+            id: message.id,
+            method: message.method,
+            body: Buffer.from(`Unknown method ${message.method}`),
+          });
+        }
+        
+        res.set('Content-Type', 'application/octet-stream');
+        res.send(RpcMessage.encode(response).finish());
+        });
+        app.listen(port, () => {
+          console.log(`Server listening on port ${port}`);
+          });
+        }
